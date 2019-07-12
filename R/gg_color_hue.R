6f7abb8907dff3459660d35d4f8957d8b2e77eef
Gunther Glehr <gunther.glehr@ukr.de>
2019-07-12 11:12:28 +0200

    gg_color_hue

---
+++ R/gg_color_hue.R
@@ -0,0 +1,15 @@
+#' ggplot color hue
+#'
+#' This generates the default ggplot colors
+#'
+#' @param n
+#' How many colors you want
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gg_color_hue <- function(n) {
+	hues = seq(15, 375, length = n + 1)
+	hcl(h = hues, l = 65, c = 100)[1:n]
+}
