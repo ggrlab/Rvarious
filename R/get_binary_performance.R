423042332a3402be654ffbc4d6613b8b63c3e8b9
GuntherG <gunthergl@gmx.net>
2021-06-30 20:38:42 +0200

    get and plot binary probabilities

---
+++ R/get_binary_performance.R
@@ -0,0 +1,60 @@
+
+#' Get binary performance
+#'
+#' Get performance measures for binary classification problems
+#'
+#' @param prob_positive
+#'   Probabilities to be in the positive class. Do not essentially have to be 0...1 values but
+#'   must be the values predicting the positive class
+#' @param class_positive
+#'   The true class values
+#' @param measures
+#'   Which measures you want to have, usually measures which are connected to a cutoff.
+#'   The measures are returned from ROCit::measureit, see further information there.
+#'   'Cutoff' is the actual cutoff value, 'Depth' what proportion of observations fall on or above
+#'   the cutoff.
+#' @param cutoff
+#'   The cutoff where you want the "measures" from.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' get_performances(
+#' 	prob_positive = 1:100,
+#' 	class = c(rep(0, 50), rep(1, 50))
+#' )
+#' get_performances(
+#' 	prob_positive = 1:100,
+#' 	class = c(rep(0, 50), rep(1, 50)),
+#' 	cutoff = 50
+#' )
+#' get_performances(
+#' 	prob_positive = (1:100)/100,
+#' 	class = c(rep(0, 50), rep(1, 50)),
+#' 	cutoff = .50
+#' )
+
+get_binary_performance <- function(
+	prob_positive,
+	class,
+	measures=c('ACC', 'SPEC', 'SENS'),
+	cutoff=0.5
+){
+	rocit_perf <- ROCit::rocit(
+		score=prob_positive,
+		class=class
+	)
+	# Measureit returns all possible cutoffs (or atleast many)
+	rocit_singleperf <- ROCit::measureit(
+		rocit_perf,
+		measure=measures
+	)
+	rocit_singleperf <- data.frame(do.call(cbind, rocit_singleperf))
+	# Get the first cutoff which is smaller than the prospected cutoffs
+	# as the cutoffs are sorted decreasingly
+	cutoff_index <- which(rocit_singleperf$Cutoff <= cutoff)[1]
+	rocit_singleperf <- rocit_singleperf[cutoff_index, ]
+	return(list('perf'=rocit_perf, 'cutoff_measures'=rocit_singleperf))
+}
---
+++ R/plot_probability_pred_true.R
@@ -0,0 +1,111 @@
+
+#' Title
+#'
+#' @param predictions
+#' @param trues
+#' @param names
+#' @param truenames
+#' @param maintitle
+#' @param cutoff
+#' @param probabilities
+#' @param traintest
+#' @param blackwhite
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' set.seed(17)
+#' traintest <- sample(c("train", "test"), 100, replace = TRUE)
+#' plot_probability_pred_true(predictions = 1:100
+#' 						   ,trues = c(rep(0, 50), rep(1, 50))
+#' 						   ,names = paste0("S", 1:100)
+#' 						   ,truenames=c("1"="infected", "0"="non.infected")
+#' 						   ,maintitle = "CRP"
+#' 						   ,cutoff = 60
+#' 						   ,probabilities = FALSE
+#' 						   ,traintest=traintest
+#' 						   ,blackwhite = TRUE
+#' )
+
+
+plot_probability_pred_true <- function(
+	predictions, trues, names, truenames=c("1"="infected", "0"="non.infected")
+	,maintitle="", cutoff=.5, probabilities=TRUE, traintest, blackwhite=FALSE){
+	tmp.res <- tibble::tibble(
+		"sample"=names
+		,"prob.positiveclass"=predictions
+		,"trueValues"=trues
+		,"trueClass"=truenames[as.character(trues)]
+		)
+	if(!missing(traintest) && length(traintest) == nrow(tmp.res)){
+		tmp.res$traintest <- traintest
+	}
+
+	tmp.res <- dplyr::arrange(tmp.res, prob.positiveclass)
+	tmp.res$sample <- factor(tmp.res$sample, levels=tmp.res$sample)
+
+	if(blackwhite){
+		tmp.res$sampletype <- paste0(tmp.res$trueClass, "_", tmp.res$traintest)
+		plot00 <- ggplot2::ggplot(tmp.res, ggplot2::aes(x=sample, y=prob.positiveclass, shape=sampletype))+
+			ggplot2::scale_shape_manual(values=c(17, 2, 16, 1))
+
+	}else{
+		plot00 <- ggplot2::ggplot(tmp.res, ggplot2::aes(x=sample, y=prob.positiveclass, shape=traintest, col=trueClass))+
+			ggplot2::scale_shape_manual(values=c(17, 2))
+	}
+	plot00 <- plot00 +
+		ggplot2::geom_point(na.rm = TRUE)+
+		ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust=.5, size=7)
+			  ,legend.position = "top")+
+		ggplot2::geom_hline(yintercept = cutoff)
+
+
+	if(probabilities){
+		plot00 <- plot00 +
+			ggplot2::scale_y_continuous(sec.axis =
+											ggplot2::sec_axis(~ .
+							   			 , breaks = c(1, 0), labels=c("certainly Infected", "certainly not infected"))
+							   , limits = c(0,1))
+	}
+	browser()
+	rocit_perf <- get_binary_performance(
+		prob_positive = tmp.res$prob.positiveclass,
+		class = tmp.res$trueValues,
+		cutoff = cutoff
+	)
+
+	perf_subtitle_all <- paste0(names(rocit_perf$cutoff_measures), "=", round(rocit_perf$cutoff_measures, 3), collapse = "  ")
+	perf_subtitle <- perf_subtitle_all
+	perf_list <- list("allsamples"=rocit_perf)
+	if(!missing(traintest) && length(traintest) == nrow(tmp.res)){
+		res.train <- tmp.res[tmp.res$traintest == "train", ]
+		res.test <- tmp.res[tmp.res$traintest == "test", ]
+		perf_train <- get_binary_performance(
+			prob_positive = res.train$prob.positiveclass
+			,class = res.train$trueValues
+			,cutoff = cutoff)
+		perf_test <- get_binary_performance(
+			prob_positive = res.test$prob.positiveclass
+			,class = res.test$trueValues
+			,cutoff = cutoff)
+		perf_subtitle <- paste0("only test:   ", paste0(names(perf_test$cutoff_measures), "=", round(perf_test$cutoff_measures, 3), collapse = "  "))
+		perf_list <- c(perf_list, list("train"=perf_train, "test"=perf_test))
+	}
+
+	plot00 <- plot00 + ggplot2::ggtitle(label = maintitle
+							   ,subtitle = perf_subtitle)
+
+
+	ROCR::plot(perf_list$allsamples$perf, main=maintitle, sub=paste0("all: ", perf_subtitle_all))
+	perf_list <- c(perf_list, "cutoff"=cutoff)
+	return(
+		list("perf"=perf_list,
+			 "predplot"=plot00,
+			 'ROCcurve'=ROCit:::plot.rocit(perf_list$allsamples$perf)
+		)
+	)
+}
+
+
