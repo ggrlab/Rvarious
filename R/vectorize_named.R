a83c67503092371ee97df76e0b97882342430efa
gunthergl_r2 <gunthergl@gmx.net>
2022-01-24 12:44:08 +0100

    R/vectorize_named: update examples

--- R/vectorize_named.R
+++ R/vectorize_named.R
@@ -10,7 +10,18 @@
 #' @export
 #'
 #' @examples
-#' 
+#' mymat <- matrix(1:6, nrow = 6)
+#' vectorize_named(mymat)
+#'
+#' rownames(mymat) <- paste0('f_', 1:6)
+#' vectorize_named(mymat)
+#'
+#' mymat <- matrix(1:6, nrow = 3)
+#' # This here should fail because there is more than 1 column in the matrix mymat!
+#' vectorize_named(mymat)
+
+
+
 vectorize_named <- function(named_matrix) {
     if(ncol(named_matrix) > 1)
         stop('Only a matrix with a single column is allowed')
@@ -18,12 +29,3 @@ vectorize_named <- function(named_matrix) {
     names(new_vec) <- rownames(named_matrix)
     return(new_vec)
 }
-mymat <- matrix(1:6, nrow = 6)
-vectorize_named(mymat)
-
-rownames(mymat) <- paste0('f_', 1:6)
-vectorize_named(mymat)
-
-mymat <- matrix(1:6, nrow = 3)
-# This here should fail because there is more than 1 column in the matrix mymat!
-vectorize_named(mymat)
