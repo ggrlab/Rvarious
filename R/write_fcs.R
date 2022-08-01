0a80cce0b3efffb44eeff5a355320257d09c7ba4
gunthergl <gunthergl@gmx.net>
2022-08-01 14:10:38 +0200

    update write_fcs

--- R/write_fcs.R
+++ R/write_fcs.R
@@ -1,7 +1,20 @@
+#' Export a matrix to an .fcs file
+#'
+#' It adds (and removes) two cells with constant maximum and minimum values to have nice visual
+#' visual ranges for Kaluza
+#'
+#' @param cell_matrix
+#' Matrix with cols=Samples and rows=features
+#' @param filepath
+#' Where the .fcs gets saved
+#' @param max_val
+#' The fake cell with maximum values which gets added and after flowFrame creation removed
+#' @param min_val
+#' The fake cell with miminum values which gets added and after flowFrame creation removed
+#' @return NULL
+#' @export
+#'
 write_fcs <- function(cell_matrix, filepath, max_val = 1e7, min_val = -1e3) {
-    # n cells = rows
-    # p markers = columns
-
     cell_matrix <- rbind(cell_matrix, max_val, min_val)
     ff <- flowCore::flowFrame(cell_matrix)
     ff <- ff[-nrow(ff), ]
